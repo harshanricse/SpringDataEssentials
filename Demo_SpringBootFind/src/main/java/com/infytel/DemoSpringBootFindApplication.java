497bb6b606d530661fc41195fe2d126b9de0200b
harshanricse <tunuguntlaharsha@gmail.com>
2025-05-04 16:13:55 +0530

    updated db creds and removed log4j dependency

--- Demo_SpringBootFind/src/main/java/com/infytel/DemoSpringBootFindApplication.java
+++ Demo_SpringBootFind/src/main/java/com/infytel/DemoSpringBootFindApplication.java
@@ -27,46 +27,24 @@ public class DemoSpringBootFindApplication implements CommandLineRunner{
 		Log logger = LogFactory.getLog(getClass());
 		Scanner scanner = new Scanner(System.in);
 		CustomerDTO customer1= new CustomerDTO(7022713754L, "Adam", 27, 'M', "Chicago", 1); 
-
 		CustomerDTO customer2= new CustomerDTO(7022713744L, "Susan", 27, 'F', "Alberta", 2); 
-
 		CustomerDTO customer3= new CustomerDTO(7022713722L, "Lucy", 27, 'F', "MUMBAI", 3); 
-
 		//Now let us invoke service layer method to insert Customer 
-
 		service.insertCustomer(customer1); 
-
 		service.insertCustomer(customer2); 
-
 		service.insertCustomer(customer3); 
-
-		System.out.println("Records added successfully."); 
-		
-		 
-         logger.info("Let's print the details of a Customer"); 
-
-         System.out.println("Enter the phone Number of the Customer whose details have to be printed."); 
-
-         Long phoneNo1 = scanner.nextLong(); 
-
-         CustomerDTO customerDTO = service.getCustomer(phoneNo1); 
-
-         logger.info("Customer Details:"); 
-
-         logger.info("Phone Number : "+customerDTO.getPhoneNumber()); 
-
-         logger.info("Name         : "+customerDTO.getName()); 
-
-         logger.info("Age          : "+customerDTO.getAge()); 
-
-         logger.info("Gender       : "+customerDTO.getAge()); 
-
-         logger.info("Address      : "+customerDTO.getAddress()); 
-
-         logger.info("Plan ID      : "+customerDTO.getPlanId()); 
-
-         scanner.close(); 
-		
+		System.out.println("Records added successfully."); 			 
+        logger.info("Let's print the details of a Customer"); 
+        System.out.println("Enter the phone Number of the Customer whose details have to be printed."); 
+        Long phoneNo1 = scanner.nextLong(); 
+        CustomerDTO customerDTO = service.getCustomer(phoneNo1); 
+        logger.info("Customer Details:"); 
+        logger.info("Phone Number : "+customerDTO.getPhoneNumber()); 
+        logger.info("Name         : "+customerDTO.getName()); 
+        logger.info("Age          : "+customerDTO.getAge()); 
+        logger.info("Gender       : "+customerDTO.getAge()); 
+        logger.info("Address      : "+customerDTO.getAddress());
+        logger.info("Plan ID      : "+customerDTO.getPlanId()); 
+        scanner.close(); 		
 	}
-
 }
--- Demo_SpringDataInsert/src/main/java/com/infytel/DemoSpringDataInsertApplication.java
+++ Demo_SpringDataInsert/src/main/java/com/infytel/DemoSpringDataInsertApplication.java
@@ -25,23 +25,12 @@ public class DemoSpringDataInsertApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		CustomerDTO customer1= new CustomerDTO(7022713754L, "Adam", 27, 'M', "Chicago", 1); 
-
 		CustomerDTO customer2= new CustomerDTO(7022713744L, "Susan", 27, 'F', "Alberta", 2); 
-
 		CustomerDTO customer3= new CustomerDTO(7022713722L, "Lucy", 27, 'F', "MUMBAI", 3); 
-
 		//Now let us invoke service layer method to insert Customer 
-
 		customerService.insertCustomer(customer1); 
-
 		customerService.insertCustomer(customer2); 
-
 		customerService.insertCustomer(customer3); 
-
-		System.out.println("Records added successfully."); 
-
-		 
-		
+		System.out.println("Records added successfully."); 	
 	}
-
 }
